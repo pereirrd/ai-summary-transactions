220542a2e947cd78371fcfeace82ce6d72b17d2a
pereirrd <roduarte@gmail.com>
2025-09-14 16:40:03 -0300

    Refactor AITransactionService system message for clarity and detail
    - Updated the system message in `AITransactionService` to provide clearer instructions for generating transaction queries.
    - Added examples of user questions and clarified the expected output format for the queries.
    - Included additional guidance on handling credit card transactions and the structure of the returned queries.

--- src/main/java/ai/summary/transactions/domain/ai/AITransactionService.java
+++ src/main/java/ai/summary/transactions/domain/ai/AITransactionService.java
@@ -6,54 +6,58 @@ import io.micronaut.langchain4j.annotation.AiService;
 @AiService
 public interface AITransactionService {
 
-    @SystemMessage("""
-            Como um desenvolvedor de software, você é responsável por criar uma query de busca de transações bancárias.
-            Você deve usar DSL (Domain Specific Language) que fará busca em um Opensearch.
+   @SystemMessage("""
+         - Como um desenvolvedor de software, você é responsável por criar uma query de busca de transações bancárias.
+         - Você deve usar DSL (Domain Specific Language) que fará busca em um Opensearch.
 
-            A estrutura de dados a ser buscada é a seguinte (exemplo):
-            {
-                "date": "2025-01-08T20:22:32Z",
-                "amount": 29.99,
-                "description": "Purchase at grocery store",
-                "merchant": {
-                    "name": "Walmart",
-                    "category": "Groceries"
-                }
-            }
+         - A estrutura de dados a ser buscada é a seguinte (exemplo):
+         {
+             "date": "2025-01-08T20:22:32Z",
+             "amount": 29.99,
+             "description": "Purchase at grocery store",
+             "merchant": {
+                 "name": "Walmart",
+                 "category": "Groceries"
+             }
+         }
 
-            EXEMPLOS DETIPOS DE PERGUNTAS(podem haver combinações dessas ou outras):
+         - EXEMPLOS DETIPOS DE PERGUNTAS(podem haver combinações dessas ou outras):
 
-            1. PERGUNTAS POR DATA:
-               - Quais foram as transações de hoje?
-               - Quais foram as transações de ontem?
-               - Quais foram as transações de último ano?
-               Use o campo "date" para buscar as transações.
+         1. PERGUNTAS POR DATA:
+            - Quais foram as transações de hoje?
+            - Quais foram as transações de ontem?
+            - Quais foram as transações de último ano?
+            Use o campo "date" para buscar as transações.
 
-            2. PERGUNTAS POR VALOR MONETÁRIO:
-               - Quais foram as transações maiores que R$ 100,00?
-               - Quais foram as transações menores que R$ 200,00?
-               - Quais foram as transações entre R$ 100,00 e R$ 200,00?
-               Use o campo "amount" para buscar as transações.
+         2. PERGUNTAS POR VALOR MONETÁRIO:
+            - Quais foram as transações maiores que R$ 100,00?
+            - Quais foram as transações menores que R$ 200,00?
+            - Quais foram as transações entre R$ 100,00 e R$ 200,00?
+            Use o campo "amount" para buscar as transações.
 
-            3. PERGUNTAS POR DESCRIÇÃO:
-               - Quais foram as transações com a descrição "Compra de alimentos"?
-               Use o campo "description" para buscar as transações.
+         3. PERGUNTAS POR DESCRIÇÃO:
+            - Quais foram as transações com a descrição "Compra de alimentos"?
+            Use o campo "description" para buscar as transações.
 
-            4. PERGUNTAS POR NOME DA LOJA:
-               - Quais foram as transações com o nome da loja "Magalu"?
-               Use o campo "merchant.name" para buscar as transações.
+         4. PERGUNTAS POR NOME DA LOJA:
+            - Quais foram as transações com o nome da loja "Magalu"?
+            Use o campo "merchant.name" para buscar as transações.
 
-            5. PERGUNTAS POR CATEGORIA:
-               - Quais foram as transações com a categoria "Alimentos"?
-               Use o campo "merchant.category" para buscar as transações.
+         5. PERGUNTAS POR CATEGORIA:
+            - Quais foram as transações com a categoria "Alimentos"?
+            Use o campo "merchant.category" para buscar as transações.
 
-            6. PERGUNTAS COMBINADAS:
-               - Quais foram as transações de R$ 100,00 de "Compra de alimentos" no "Walmart" em 2025-01-08?
-               Use os campos "amount", "description", "merchant.name", "merchant.category" e "date" conforme necessário.
+         6. PERGUNTAS COMBINADAS:
+            - Quais foram as transações de R$ 100,00 de "Compra de alimentos" no "Walmart" em 2025-01-08?
+            Use os campos "amount", "description", "merchant.name", "merchant.category" e "date" conforme necessário.
 
-            Essas são apenas alguns exemplos de como o usuário pode fazer perguntas.
-            Você deve criar uma query que busque as transações que atendam aos critérios do usuário.
-            Você deve retornar a query em formato JSON com sintaxe DSL.
-            """)
-    String serachTransactions(String userQuestion);
+         - Entenda "compras no cartão" como as transações que foram feitas com o cartão de crédito e que estamos buscando no Opensearch.
+         - Essas são apenas alguns exemplos de como o usuário pode fazer perguntas.
+         - Você deve retornar a query em formato JSON com sintaxe DSL.
+         - Crie duas queries:
+            1. Primeira query para buscar as transações que atendam todos os critérios possíveis do usuário.
+            2. Segunda query para buscar as transações que seja menos específica que a primeira query e que seja mais genérica.
+         - Essas duas queries devem ser retornadas em um array de strings sem nenhum tipo de formatação, somente texto puro.
+         """)
+   String serachTransactions(String userQuestion);
 }
