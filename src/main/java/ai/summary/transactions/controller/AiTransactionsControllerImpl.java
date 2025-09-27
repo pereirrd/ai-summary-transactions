f144220ff79c4003fff160cd0d84de2492e6807e
pereirrd <roduarte@gmail.com>
2025-09-27 10:27:51 -0300

    Implement AI transaction insights processing and enhance Swagger documentation
    - Added `AITransactionInsightsService` interface for generating insights based on scenarios and date ranges.
    - Updated `AISummaryTransactionApp` to include a new method `processByInsights` for handling insights requests.
    - Modified `AiTransactionsControllerImpl` to support a new endpoint for retrieving AI transaction insights.
    - Enhanced Swagger documentation to include new parameters and responses for the insights feature, improving API clarity.

--- src/main/java/ai/summary/transactions/controller/AiTransactionsControllerImpl.java
+++ src/main/java/ai/summary/transactions/controller/AiTransactionsControllerImpl.java
@@ -1,6 +1,7 @@
 package ai.summary.transactions.controller;
 
-import ai.summary.transactions.model.ProcessAITransactionSummary200Response;
+import ai.summary.transactions.model.AIResultResponse;
+import ai.summary.transactions.model.GetAITransactionInsightsScenarioParameter;
 import ai.summary.transactions.model.ProcessAITransactionSummaryRequest;
 
 import java.time.LocalDate;
@@ -21,14 +22,14 @@ public class AiTransactionsControllerImpl implements AiTransactionsApi {
     private final AISummaryTransactionApp aiTransactionApplication;
 
     @Override
-    public HttpResponse<@Valid ProcessAITransactionSummary200Response> processAITransactionSummary(
+    public HttpResponse<@Valid AIResultResponse> processAITransactionSummary(
             @NotNull LocalDate startDate, @NotNull LocalDate endDate,
             @NotNull @Valid ProcessAITransactionSummaryRequest processAITransactionSummaryRequest) {
         try {
             String result = aiTransactionApplication
                     .processByList(processAITransactionSummaryRequest.getQuestion());
 
-            var response = new ProcessAITransactionSummary200Response()
+            var response = new AIResultResponse()
                     .result(result);
 
             return HttpResponse.ok(response);
@@ -37,6 +38,14 @@ public class AiTransactionsControllerImpl implements AiTransactionsApi {
                     startDate, endDate, processAITransactionSummaryRequest.getQuestion(), exception);
             return HttpResponse.serverError();
         }
+    }
+
+    @Override
+    public HttpResponse<@Valid AIResultResponse> getAITransactionInsights(
+            @NotNull GetAITransactionInsightsScenarioParameter scenario, @NotNull LocalDate startDate,
+            @NotNull LocalDate endDate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAITransactionInsights'");
     }
 
 }
